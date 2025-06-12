1c1fdd9bcc7c5f352442ff4f9d678139de503724
gopika <gopikac680@gmail.com>
2025-06-12 23:42:43 +0530

    add

--- my-app/src/Hook/useListenMessages.jsx
+++ my-app/src/Hook/useListenMessages.jsx
@@ -1,16 +1,16 @@
 import { useEffect } from 'react';
-import {  useSocketContext } from '../context/SocketContext';
-import {useConversation} from '../context/ConversationContext'
+import { useConversation } from '../context/ConversationContext';
+
+import {useSocketContext} from '../context/socketContext';
+
 
 const useListenMessages = () => {
 const {socket}=useSocketContext()
-console.log(socket,"//////////////////////////")
 const {messages,setMessages} =useConversation();
 
 
 useEffect(()=>{
 socket?.on("newMessage",(newMessage)=>{
-    console.log(newMessage,"newMessageeeee")
 setMessages([...messages,newMessage])
 },[messages,setMessages,socket])
 
