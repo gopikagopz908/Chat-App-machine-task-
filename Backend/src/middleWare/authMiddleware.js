17e59e116d8e9ab92d8e831566a4db7aeddf3ab8
gopika <gopikac680@gmail.com>
2025-06-13 15:41:45 +0530

    add

--- Backend/src/middleWare/authMiddleware.js
+++ Backend/src/middleWare/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const protect = (req, res, next) => {
   const token = req.cookies?.token;
-
+console.log(token)
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized: No token in cookies' });
   }
